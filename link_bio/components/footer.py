69d47677ab77ea19486013fc954568ba08ab7ca5
AndresDev007 <qu4rtz.neu7ryn0@outlook.com>
2024-11-06 06:15:26 -0300

    primer commit

---
+++ link_bio/components/footer.py
@@ -0,0 +1,47 @@
+import reflex as rx
+import datetime
+import link_bio.constants as const
+from link_bio.styles.styles import Size
+from link_bio.styles.colors import Color, TextColor
+
+
+def footer() -> rx.Component:
+    return rx.vstack(
+        rx.image(
+            src="logo.png",
+            height=Size.VERY_BIG.value,
+            width=Size.VERY_BIG.value,
+            alt="Logotipo de MoureDev. Una \"eme\" entre llaves."
+        ),
+        rx.vstack(
+            rx.box(
+                f"© 2014-{datetime.date.today().year} ",
+                rx.link(" MoureDev by Brais Moure ", color=Color.PRIMARY.value, href=const.MOUREDEV_URL, is_external=True),
+                " v3.",
+                padding_top=Size.DEFAULT.value
+            ),
+            font_size=Size.MEDIUM.value
+        ),
+        rx.link(
+            rx.hstack(
+                rx.image(
+                    src="icons/github.svg",
+                    height=Size.LARGE.value,
+                    width=Size.LARGE.value
+                ),
+                rx.text(
+                    "BUILDING SOFTWARE WITH ♥ FROM GALICIA TO THE WORLD.",
+                    font_size=Size.MEDIUM.value,
+                    margin_top=Size.ZERO.value
+                )
+            ),
+            href=const.REPO_URL,
+            is_external=True
+        ),
+        margin_bottom=Size.BIG.value,
+        padding_bottom=Size.BIG.value,
+        padding_x=Size.BIG.value,
+        spacing=Size.ZERO.value,
+        color=TextColor.FOOTER.value,
+        align="center"
+    )
---
+++ link_bio/link_bio.py
@@ -0,0 +1,67 @@
+import reflex as rx
+import link_bio.styles.styles as styles
+from link_bio.components.navbar import navbar
+from link_bio.components.footer import footer
+from link_bio.views.header import header
+from link_bio.views.links import links
+from link_bio.views.sponsors import sponsors
+from link_bio.styles.styles import Size
+
+
+# class State(rx.State):
+#     pass
+
+
+def index() -> rx.Component:
+    return rx.box(
+        rx.script("document.documentElement.lang='es'"),
+        navbar(),
+        rx.center(
+            rx.vstack(
+                header(),
+                links(),
+                sponsors(),
+                max_width=styles.MAX_WIDTH,
+                width="100%",
+                margin_y=Size.BIG.value,
+                padding=Size.BIG.value
+            )
+        ),
+        footer()
+    )
+
+
+app = rx.App(
+    stylesheets=styles.STYLESHEETS,
+    style=styles.BASE_STYLE,
+    # head_components=[
+    #     rx.script(src="https://www.googletagmanager.com/gtag/js?id=G-3YGHT3XJFS"),
+    #     rx.script(
+    #         """
+    #         window.dataLayer = window.dataLayer || [];
+    #         function gtag(){dataLayer.push(arguments);}
+    #         gtag('js', new Date());
+    #         gtag('config', 'G-3YGHT3XJFS');
+    #         """
+    #     ),
+    # ],
+)
+
+title="MoureDev | Te enseño programación y desarrollo de software"
+description="Hola, mi nombre es Brais Moure. Soy ingeniero de software, desarrollador freelance full-stack y divulgador."
+preview="https://moure.dev/preview.jpg"
+
+app.add_page(
+    index,
+    title=title,
+    description=description,
+    image=preview,
+    meta = [
+        {"name": "og:type", "content": "website"},
+        {"name": "og:title", "content": title},
+        {"name": "og:description", "content": description},
+        {"name": "og:image", "content": preview},
+        {"name": "twiter:card", "content": "summary_large_image"},
+        {"name": "twiter:site", "content": "@mouredev"}
+    ]
+)
